fe18b65ba94f1afbf33e7ea97f15d2e43bc657c1
GaneshRoyal07 <kondetiganesh43@gmail.com>
2026-03-17 17:50:38 +0530

    add page component

--- src/App.js
+++ src/App.js
@@ -1,64 +1,8 @@
-import React, { useState, useEffect } from "react";
-import UserTable from "./components/UserTable";
-import UserForm from "./components/UserForm";
-import { getUsers, addUser, updateUser, deleteUser } from "./services/api";
+import React from "react";
+import UserPage from "./pages/UserPage";
 
 function App() {
-
-const [users, setUsers] = useState([]);
-const [editingUser, setEditingUser] = useState(null);
-
-const loadUsers = async () => {
-const res = await getUsers();
-setUsers(res.data);
-};
-
-useEffect(() => {
-loadUsers();
-}, []);
-
-const handleAddUser = async (user) => {
-await addUser(user);
-loadUsers();
-};
-
-const handleDeleteUser = async (id) => {
-await deleteUser(id);
-loadUsers();
-};
-
-const handleEditUser = (user) => {
-setEditingUser(user);
-};
-
-const handleUpdateUser = async (id, user) => {
-await updateUser(id, user);
-setEditingUser(null);
-loadUsers();
-};
-
-return (
-
-<div style={{ width: "600px", margin: "auto" }}>
-
-<h1>User CRUD Management</h1>
-
-<UserForm
-addUser={handleAddUser}
-updateUser={handleUpdateUser}
-editingUser={editingUser}
-/>
-
-<UserTable
-users={users}
-deleteUser={handleDeleteUser}
-editUser={handleEditUser}
-/>
-
-</div>
-
-);
-
+  return <UserPage />;
 }
 
 export default App;
---
+++ src/pages/UserPage.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import UserTable from "../components/UserTable";
+import UserForm from "../components/UserForm";
+import { getUsers, addUser, updateUser, deleteUser } from "../services/api";
+
+function UserPage() {
+
+  const [users, setUsers] = useState([]);
+  const [editingUser, setEditingUser] = useState(null);
+
+  const loadUsers = async () => {
+    const res = await getUsers();
+    setUsers(res.data);
+  };
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  const handleAddUser = async (user) => {
+    await addUser(user);
+    loadUsers();
+  };
+
+  const handleDeleteUser = async (id) => {
+    await deleteUser(id);
+    loadUsers();
+  };
+
+  const handleEditUser = (user) => {
+    setEditingUser(user);
+  };
+
+  const handleUpdateUser = async (id, user) => {
+    await updateUser(id, user);
+    setEditingUser(null);
+    loadUsers();
+  };
+
+  return (
+    <div style={{ width: "600px", margin: "auto" }}>
+      <h1>User CRUD Management</h1>
+
+      <UserForm
+        addUser={handleAddUser}
+        updateUser={handleUpdateUser}
+        editingUser={editingUser}
+      />
+
+      <UserTable
+        users={users}
+        deleteUser={handleDeleteUser}
+        editUser={handleEditUser}
+      />
+    </div>
+  );
+}
+
+export default UserPage;
